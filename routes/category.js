df440456a7897158d6bde684700ceb59eb0bb60f
Jerome Rodriguez <jerome.rodriguez@torontomu.ca>
2025-01-30 13:46:57 -0500

    api kinda working

--- routes/category.js
+++ routes/category.js
@@ -3,6 +3,7 @@ const router = express.Router();
 import fs from "fs";
 import { v4 as uuidv4 } from "uuid";
 
+// /category
 router.get("/", (req, res) => {
   res.send("This is the /category base");
 });
--- routes/events.js
+++ routes/events.js
@@ -3,8 +3,42 @@ const router = express.Router();
 import fs from "fs";
 import { v4 as uuidv4 } from "uuid";
 
+router.use(express.json());
+
+function readEvents() {
+  const eventsData = fs.readFileSync("./data/events.json");
+  const parsedData = JSON.parse(eventsData);
+  return parsedData;
+}
+
+// /events
 router.get("/", (req, res) => {
-  res.send("This is the /events base");
+  const events = readEvents();
+  res.json(events);
+});
+
+// POST /events
+router.post("/", (req, res) => {
+  const events = readEvents();
+
+  const newEvent = {
+    id: uuidv4(),
+    name: req.body.name,
+    photo: "./public/photos/event-img.jpg",
+    date: req.body.date,
+    time: req.body.time,
+    location: req.body.location,
+    category: req.body.category, // drop down selector
+    description: req.body.description,
+    price: req.body.price,
+  };
+
+  console.log(req.body);
+  // events.unshift(newEvent);
+
+  // fs.writeFileSync("./data/events.json"), JSON.stringify(events);
+
+  // res.json(newEvent);
 });
 
 export default router;
