9f45e4bcb827f7da233bf987fc08576a1ce982c7
Gabriela <gcdbarreira@gmail.com>
2025-01-30 11:38:35 -0500

    initial endpoint setup

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ import express from "express";
 const app = express();
 import "dotenv/config";
 import cors from "cors";
+import events from "./routes/events.js";
+import category from "./routes/category.js";
 
 const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/events", events);
+app.use("/category", category);
 
 app.listen(PORT, () => {
   console.log("Server is listening on port " + PORT);
