15904195080134d3f1b223134f7a010293e43466
Jerome Rodriguez <jerome.rodriguez@torontomu.ca>
2025-02-15 16:54:46 -0500

    changed cors

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ import fs from "fs";
 
 const PORT = process.env.PORT;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://project-fomo.netlify.app", // Replace with your actual Netlify URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+// app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
